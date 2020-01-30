ecaef8944aa763c53ea293f56b20125fd24c4aaf
yunom <1435844730@qq.com>
2020-01-30 10:07:39 -0600

    update to the completed hmk

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,99 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  #arr.inject(0){|s,x| s + x }
+  if arr.empty?
+    0
+  else
+    arr.reduce(:+)
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    0
+  else
+    a = arr.max
+    arr.delete_at(arr.index(arr.max))
+    if arr.empty?
+      a
+    else
+      b = arr.max
+      a+b
+    end
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  r = false
+  for i in arr do
+    index = arr.index(i)
+    arr.delete_at(arr.index(i))
+    if arr.include?(n-i)
+      r = true
+    end
+    arr.insert(index, i)
+  end
+  r
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s[0] == 'a' || s[0] == 'A' || s[0] == 'E' || s[0] == 'e' || s[0] == 'I' || s[0] == 'i' || s[0] == 'o' || s[0] == 'O' || s[0] == 'U' || s[0] == 'u' || s.empty? || s[0].count("a-zA-Z") < 1
+    false
+  else
+    true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  puts s
+  if s.empty?
+    false
+  elsif s.count('01') == s.size
+    a = s.to_i(2)
+    if (a%4) == 0
+      true
+    else
+      false
+    end
+  else
+    false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price) 
+    if isbn.empty?
+      raise ArgumentError, 'isbn is empty'
+    elsif price>0
+      @isbn = String.new(isbn)
+      @price = price 
+    else
+      raise ArgumentError, 'price too low'
+    end 
+  end
+  def isbn
+    @isbn
+  end
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  def price
+    @price
+  end
+  def price=(price)
+    @price = price
+  end 
+  def price_as_string
+    sprintf("$"+"%.2f", price)
+  end 
+  
 end
